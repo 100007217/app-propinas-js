efb1deda0451875834e28d0f355a11c3f6b8a019
100007217 <gomezmonterroso14@gmail.com>
2021-10-08 19:51:18 +0200

    Commit final de coger elementos del HTML de difs maneras

--- js/code.js
+++ js/code.js
@@ -1,8 +1,12 @@
 function calculo() {
     alert("Danny Larrea es genial");
     let totalCuenta = document.getElementById('total').value;
-    alert(totalCuenta);
-    alert("Danny Larrea es genial3");
+    let servicio = document.getElementById('servicio').value;
+    let personas = document.getElementById('personas').value;
+
+    /* Las clases y los nombre requieren un numero porque no son unicas, asi como los ID si lo son
     let servicio = document.getElementsByName('servicio')[0].value;
-    alert(servicio);
+    let personas = document.getElementsByClassName('personas')[0].value; */
+    alert(personas + " " + servicio + " " + totalCuenta)
+
 }
