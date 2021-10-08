ac509d8017e862b003b1a5161b579ef403ce8522
100007217 <gomezmonterroso14@gmail.com>
2021-10-08 19:41:31 +0200

    Añadido en el JS elementos recibidos desde HTML

--- js/code.js
+++ js/code.js
@@ -1,3 +1,8 @@
 function calculo() {
     alert("Danny Larrea es genial");
+    let totalCuenta = document.getElementById('total').value;
+    alert(totalCuenta);
+    alert("Danny Larrea es genial3");
+    let servicio = document.getElementsByName('servicio')[0].value;
+    alert(servicio);
 }
